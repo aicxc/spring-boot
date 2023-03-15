5c926102da1613da8476c0e96982bb9a8dda8bde
aicxc <15999966670@163.com>
2023-03-15 13:57:17 +0800

    测试一下

--- spring-boot-local/src/main/java/com/spring/boot/local/SpringBootLocalApplication.java
+++ spring-boot-local/src/main/java/com/spring/boot/local/SpringBootLocalApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootLocalApplication {
 
 	public static void main(String[] args) {
+		// 测试一下
 		SpringApplication.run(SpringBootLocalApplication.class, args);
 	}
 
